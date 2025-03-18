554a4966ee3e4300ab2733042b57b005a27cf089
Alex <alejandro.b.mtz.94@gmail.com>
2025-03-17 19:58:00 -0600

    Variable amigos e insertar amigo

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+
+    if (amigo.length == 0) {
+        alert("Por Favor Inserte Un Nombre");
+    } else {
+        amigos.push(amigo);
+    }
+}
